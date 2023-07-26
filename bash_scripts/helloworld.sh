03a5f7e4a6d17c4e54bd9282e60094403d1b1588
Ishani Ganguly <ishaniganguly@gmail.com>
2023-07-25 19:28:08 -0700

    bash hello world

---
+++ bash_scripts/helloworld.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+#
+# Simple "Hello World" submit script for Slurm.
+#
+#SBATCH --job-name=HelloWorld 
+#SBATCH -c 1                   
+#SBATCH --time=1:00             
+#SBATCH --mem-per-cpu=1gb       
+#SBATCH --gres=gpu:gtx1080:1   
+ 
+echo "Hello World"
+sleep 10
+date
+ 
+# End of script
