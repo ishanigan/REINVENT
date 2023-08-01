fb81373ddc42ee52d6da8acef46052abe2724974
Ishani Ganguly <ishaniganguly@gmail.com>
2023-08-01 16:01:01 -0700

    Update reinvent_og_cpu.sh

--- bash_scripts/reinvent_og_cpu.sh
+++ bash_scripts/reinvent_og_cpu.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH -C cpu
 #SBATCH -q regular
-#SBATCH -J helloworld
+#SBATCH -J og_reinvent
 #SBATCH -t 01:00:00
  
 python ../main.py --scoring-function no_sulphur --num_steps 10
