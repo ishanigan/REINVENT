40145d09c834516dac9302e6bbbb034183fb25c6
ishanigan <iganguly@caltech.edu>
2023-08-21 22:06:19 -0700

    new exploration schemes

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ parser.add_argument('--num-steps', action='store', dest='n_steps', type=int,
                     default=3000)
 parser.add_argument('--batch-size', action='store', dest='batch_size', type=int,
                     default=64)
+parser.add_argument('--sigma_mode', action='store', dest='sigma_mode', type=str, choices=['static', 'linear_decay', 'exponential_decay', 'levy_flight', 'uncertainty_aware', 'uncertainty_aware_inverse', 'adaptive'], default='static')
 parser.add_argument('--sigma', action='store', dest='sigma', type=int,
                     default=20)
 #parser.add_argument('--sigma', action='store', dest='sigma', type=float,
