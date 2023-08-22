53e4afbaa547b55bd43a1e9a9fe377756ca49b97
ishanigan <iganguly@caltech.edu>
2023-08-22 11:28:24 -0700

    switch to bandgap

---
+++ reinvent_bandgap_cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -J bandgap_debug
+#SBATCH -t 01:00:00
+ 
+python ./main.py --scoring-function 'bandgap_range_soft' --num-steps 10
+ 
+# End of script
